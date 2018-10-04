1a1861de6a7feb49f3d853965f2cbb5b2062ede2
Max Curran <maxcurran96@outlook.com>
2018-10-04 18:10:33 +0100

    Basic example to validate DataFrame functionality

---
+++ src/test/scala/com/mdcurran/simple/spark/testing/DataFrameTests.scala
@@ -0,0 +1,49 @@
+package com.mdcurran.simple.spark.testing
+
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.functions.col
+import org.scalatest.FlatSpec
+
+class DataFrameTests extends FlatSpec with SharedSQLContext {
+  import DataFrameTests._
+
+  "The countOfAge function" should "return a DataFrame counting the number of records per age" in {
+    val data = Seq(
+      Row("John", 21),
+      Row("Jane", 18),
+      Row("Jim", 21),
+      Row("Jack", 19),
+      Row("Jonah", 19),
+      Row("Janet", 21))
+    val schema = Seq(
+      StructField("name", StringType),
+      StructField("age", IntegerType))
+    val df = sqlContext.createDataFrame(sqlContext.sparkContext.parallelize(data), StructType(schema))
+    val result = countOfAge(df)
+    assert(result.filter(col("age") === 21).select("count").first().mkString(",") === "3")
+  }
+
+  "The countOfAge function" should "return a row for each unique age" in {
+    val data = Seq(
+      Row("John", 21),
+      Row("Jane", 18),
+      Row("Jim", 21),
+      Row("Jack", 19),
+      Row("Jonah", 19),
+      Row("Janet", 21))
+    val schema = Seq(
+      StructField("name", StringType),
+      StructField("age", IntegerType))
+    val df = sqlContext.createDataFrame(sqlContext.sparkContext.parallelize(data), StructType(schema))
+    val result = countOfAge(df)
+    assert(result.count === 3)
+  }
+
+}
+
+object DataFrameTests {
+
+  private def countOfAge(df: DataFrame): DataFrame = df.groupBy("age").count()
+
+}
