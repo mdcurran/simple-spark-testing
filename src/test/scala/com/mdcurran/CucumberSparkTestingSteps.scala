bcbcf40e9090cfd54b58c28975cd707af4e61dbf
Max Curran <maxcurran96@outlook.com>
2018-10-21 16:36:15 +0100

    Changed SparkSession to single point of entry

--- src/test/scala/com/mdcurran/CucumberSparkTestingSteps.scala
+++
@@ -1,5 +0,0 @@
-package com.mdcurran
-
-class CucumberSparkTestingTests {
-
-}
---
+++ src/test/scala/com/mdcurran/scalatest/SharedSparkSessionTests.scala
@@ -0,0 +1,78 @@
+package com.mdcurran.scalatest
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row}
+import org.scalatest.FlatSpec
+
+class SharedSparkSessionTests extends FlatSpec with SharedSparkSession {
+  import SharedSparkSessionTests._
+
+  "The createSparkSession function" should "create an active SparkSession" in {
+    assert(!spark.sparkContext.isStopped)
+  }
+
+  "The createSparkContext function" should "have an appName of the local class" in {
+    assert(spark.sparkContext.appName == "SharedSparkSessionTests")
+  }
+
+  "The createSparkContext function" should "create a local instance of a SparkContext" in {
+    assert(spark.sparkContext.isLocal)
+  }
+
+  "The createSparkContext function" should "have the master set to local" in {
+    assert(spark.sparkContext.master == "local")
+  }
+
+  "The factorialSum function" should "return the sum of the factorial of an RDD of integers" in {
+    val rdd = spark.sparkContext.parallelize(Seq(1, 2, 3, 4, 5))
+    assert(factorialSum(rdd) == 153)
+  }
+
+  "The countOfAge function" should "return a DataFrame counting the number of records per age" in {
+    val data = Seq(
+      Row("John", 21),
+      Row("Jane", 18),
+      Row("Jim", 21),
+      Row("Jack", 19),
+      Row("Jonah", 19),
+      Row("Janet", 21))
+    val schema = Seq(
+      StructField("name", StringType),
+      StructField("age", IntegerType))
+    val df = spark.createDataFrame(spark.sparkContext.parallelize(data), StructType(schema))
+    val result = countOfAge(df)
+    assert(result.filter(col("age") === 21).select("count").first().mkString(",") === "3")
+  }
+
+  "The countOfAge function" should "return a row for each unique age" in {
+    val data = Seq(
+      Row("John", 21),
+      Row("Jane", 18),
+      Row("Jim", 21),
+      Row("Jack", 19),
+      Row("Jonah", 19),
+      Row("Janet", 21))
+    val schema = Seq(
+      StructField("name", StringType),
+      StructField("age", IntegerType))
+    val df = spark.createDataFrame(spark.sparkContext.parallelize(data), StructType(schema))
+    val result = countOfAge(df)
+    assert(result.count === 3)
+  }
+
+}
+
+object SharedSparkSessionTests {
+
+  private def factorialSum(rdd: RDD[Int]): Int = {
+    def factorial(n: Int): Int =
+      if (n == 0) 1
+      else n * factorial(n - 1)
+    rdd.map(i => factorial(i)).reduce(_ + _)
+  }
+
+  private def countOfAge(df: DataFrame): DataFrame = df.groupBy("age").count()
+
+}
